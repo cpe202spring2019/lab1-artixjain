4ffa18c4bfcc25e072987d6de46205a1ce38529b
Arti Jain <Arti@Artis-MacBook-Pro.local>
2019-04-04 19:43:03 -0700

    Location

--- location.py
+++ location.py
@@ -1,13 +1,26 @@
-# CPE 202 Lab 0
+# Name: Arti Jain         
+# Course: CPE 202
+# Instructor: Hatalsky 
+# Assignment: Lab 1 Location
+# Term: Spring 2019
 
 # represents a location using name, latitude and longitude
-class Location:
+class Location: #a location object
     def __init__(self, name, lat, lon):
         self.name = name    # string for name of location
         self.lat = lat      # latitude in degrees (-90 to 90)
         self.lon = lon      # longitude in degrees (-180 to 180)
+    #__eq__
+    def __eq__(self, other): #overrides what's in python and allows you to compare the data
+                             #isinstance tests if the objects are the same kind of objects, so if you had a Geography object with the data, it would still return false
+        return (isinstance(other, Location) & (self.name == other.name) & (self.lat == other.lat) & (self.lon == other.lon))
+    #__repr__ = how the object is represented, string representation
+    def __repr__(self):
+        string = "Location('" + self.name + "', " + str(self.lat) + ", " + str(self.lon) + ")"
+        return string
 
-# ADD BOILERPLATE HERE (__eq__ and __repr__ functions)
+# 4 basic methods every object should have
+# init, eq, repr, str
 
 def main():
     loc1 = Location("SLO", 35.3, -120.7)
@@ -21,9 +34,10 @@ def main():
     print("Location 4:",loc4)
 
     print("\nLocation 1 equals Location 2:",loc1==loc2)
-    print("Location 1 equals Location 3:",loc1==loc3)
+    print("Location 1 equals Location 3:",loc1==loc3) #Once the code changed, this went from False to True
     print("Location 1 equals Location 4:",loc1==loc4)
 
+# When the code is changed, instead of True, True, False, True, the output is all True's
     locations = [loc1, loc2]
     print(loc1 in locations)
     print(loc2 in locations)
